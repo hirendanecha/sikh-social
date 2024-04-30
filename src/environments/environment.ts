c9e6a1b117c3ef6ac5863d83d6fe90b9f7c1c2dc
vatsal <vso.opash@gmail.com>
2024-04-30 12:04:48 +0530

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+const url = 'https://api.hindu.social';
+const webUrl = 'https://hindu.social/';
+const tubeUrl = 'https://tube.hindu.social/'
+// const url = 'http://localhost:8080';
+// const webUrl = 'http://localhost:4200/';
+
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: `${url}/api/v1/`,
+  socketUrl: `${url}/`,
+  webUrl: webUrl,
+  tubeUrl: tubeUrl,
+  domain: '.healing.tube',
+  stripe_key: 'pk_test_51MwOHTSJJZB3pTDyqpZk5NJDaWkbuGiruTp79We9CcR2oODObX93TleYFTJj2qLowRuRL1DRTSIl8Vb1YiFyW7e900BYWlZKSM',
+  siteKey:'0x4AAAAAAAUxBHOVjM39yKUu',
+  secretKey:'0x4AAAAAAAUxBEy2WZ9uDMOfPPqInVM0Ixw'
+};
