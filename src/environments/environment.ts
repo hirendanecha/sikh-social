8b8e69cb86d3d5107f9e0707d3af83644c463fcc
vatsal <vso.opash@gmail.com>
2024-05-07 16:24:24 +0530

    Change APIs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-const url = 'https://api.hindu.social';
-const webUrl = 'https://hindu.social/';
-const tubeUrl = 'https://tube.hindu.social/'
+const url = 'https://api.sikh.social';
+const webUrl = 'https://sikh.social/';
+const tubeUrl = 'https://tube.sikh.social/'
 // const url = 'http://localhost:8080';
 // const webUrl = 'http://localhost:4200/';
 
@@ -11,7 +11,7 @@ export const environment = {
   socketUrl: `${url}/`,
   webUrl: webUrl,
   tubeUrl: tubeUrl,
-  domain: '.healing.tube',
+  domain: '.sikh.social',
   stripe_key: 'pk_test_51MwOHTSJJZB3pTDyqpZk5NJDaWkbuGiruTp79We9CcR2oODObX93TleYFTJj2qLowRuRL1DRTSIl8Vb1YiFyW7e900BYWlZKSM',
   siteKey:'0x4AAAAAAAUxBHOVjM39yKUu',
   secretKey:'0x4AAAAAAAUxBEy2WZ9uDMOfPPqInVM0Ixw'
